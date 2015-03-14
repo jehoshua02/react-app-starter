95a28cf9a8e0f5ae22a8b0a7db0bf29cac40b73b
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-03-13 19:04:38 -0600

    Fix Nested Route
    The app route is supposed to render it's child route using react-router
    RouteHandler. I just forgot and didn't even think about it.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,9 @@ var { Route, DefaultRoute, NotFoundRoute } = require('react-router');
 
 
 module.exports = (
-  <Route name="app" path="/">
+  <Route name="app" path="/" handler={require('./handlers/App')}>
     <DefaultRoute handler={require('./handlers/Home')} />
     <Route name="page" path="page/:id" handler={require('./handlers/Page')} />
-    <NotFoundRoute name="not-found" handler={require('./handlers/NotFound')}/>
+    <NotFoundRoute name="not-found" handler={require('./handlers/NotFound')} />
   </Route>
 );
